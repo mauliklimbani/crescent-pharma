af9b6aa6790de164628c42c45335dc7e01c2f73a
Maulik Limbani <mauliklimbani235@gmail.com>
2025-10-29 23:28:08 +0530

    Edit config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir:'build',
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true // 👈 Add this line
